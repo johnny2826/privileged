7de4e05cfe6432fa261c20cdd68bd7c93ec13522
johnny2826 <johnnyshaik1995@gmail.com>
2023-06-30 19:10:47 +0530

    privileged changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrivilegedComponent } from './privileged/privileged.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivilegedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
